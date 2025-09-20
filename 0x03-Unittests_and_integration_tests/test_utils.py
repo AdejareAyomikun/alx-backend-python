80a8d994056959470cbb163dc2dc2d127bf0e438
Adejare Faruk <nou183023868@noun.edu.ng>
2025-09-20 12:12:09 +0100

    update

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -46,8 +46,8 @@ class TestGetJson(unittest.TestCase):
         result = get_json(test_url)
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
-      
-      
+ 
+
 class TestMemoize(unittest.TestCase):
     def test_memoize(self):
         from utils import memoize
@@ -60,7 +60,7 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, "a_method", return_value=42) as mock_method:
+        with patch.object(TestClass,"a_method",return_value=42)as mock_method:
             obj = TestClass()
             self.assertEqual(obj.a_property, 42)
             self.assertEqual(obj.a_property, 42)
